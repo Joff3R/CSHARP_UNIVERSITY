5001754f231239771526f5be84cf841efb658f13
Bartosz <bkutkowski@gmail.com>
2022-01-20 00:13:29 +0100

    Add project files.

---
+++ CSHARP_UNIVERSITY/Przeciazanie_konstruktorow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP_UNIVERSITY
+{
+	internal class Przeciazanie_konstruktorow
+	{
+		public double liczba;
+		public char znak;
+
+		public Przeciazanie_konstruktorow(double first)
+		{
+		}
+
+		public Przeciazanie_konstruktorow(char first)
+		{
+		}
+
+		public Przeciazanie_konstruktorow(double first, char second)
+		{
+		}
+
+		public Przeciazanie_konstruktorow(double a, double b)
+		{
+			while (a != b)
+			{
+				if (a > b)
+					a -= b;
+				else
+					b -= a;
+			}
+			Console.WriteLine(a);
+		}
+
+		public Przeciazanie_konstruktorow(int[] tablica, int zakres, int dokad)
+		{
+			int i, j;
+
+			for (i = 1; i <= zakres; i++) tablica[i] = i;
+
+			//algorytm - sito eratostenesa
+			for (i = 2; i <= dokad; i++)
+			{
+				if (tablica[i] != 0)
+				{
+					j = i + i;
+					while (j <= zakres)
+					{
+						tablica[j] = 0;
+						j += i;
+					}
+				}
+			}
+
+
+			Console.WriteLine("Liczby pierwsze z zakresu od 1 do " + zakres);
+			for (i = 2; i <= zakres; i++)
+				if (tablica[i] != 0)
+					Console.WriteLine(i + ", ");
+		}
+
+	}
+}
---
+++ CSHARP_UNIVERSITY/Wyjatki.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP_UNIVERSITY
+{
+	internal class Wyjatki
+	{
+		public void generateArithmeticException()
+		{
+			try
+			{
+				int liczba1 = 10, liczba2 = 0;
+				Console.WriteLine(liczba1 / liczba2);
+			}
+
+			catch (ArithmeticException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		public int[] bubbleSort(int[] array)
+		{
+
+			int length = array.Length;
+
+			int temp = array[0];
+			try
+			{
+				for (int i = 0; i < length; i++)
+				{
+					for (int j = i + 1; j < length; j++)
+					{
+						if (array[i] > array[j])
+						{
+							temp = array[i];
+
+							array[i] = array[j];
+
+							array[j] = temp;
+						}
+					}
+				}
+			}
+
+			catch (IndexOutOfRangeException e)	
+			{
+				Console.WriteLine(e.Message);
+			}	
+			return array;
+		}
+
+
+	}
+}
